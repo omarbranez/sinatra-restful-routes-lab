9f2d1c5c4be7837b795aa77f695fa47e309b9250
Omar Branez <omar.branez@gmail.com>
2021-04-26 12:50:43 -0400

    commit tree 21d79eecb5b44c244059716dcafb4922a5fc700a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,52 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
+  get '/' do
+    erb :index
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+    # index action through @recipes
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+    # new action, along with lines 19-21
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+    # show action
+  end
+
+  get '/recipes/:id/edit' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    # @recipe.name = params[:name]
+    # @recipe.ingredients = params[:ingredients]
+    # @recipe.cook_time = params[:cook_time]
+    @recipe.update(params[:recipe])
+    # @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+    #edit action, along with 35-38
+  end
+
+  delete '/recipes/:id' do
+    Recipe.find(params[:id]).destroy
+    redirect to '/recipes'
+  end
+
+
 end
